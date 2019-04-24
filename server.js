f0cfc461f2e93c5132f73c18d1ce2facc6f371c9
Jared Rhodes <jaredrhodes15@gmail.com>
2019-04-23 18:14:29 -0600

    connected to db but can't load home

---
+++ server.js
@@ -0,0 +1,46 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router = express.Router();
+
+require("./config/routes")(router);
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+// To connect Handlebars to the Express app
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+
+// To use bodyParser
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
+
+// Connect Mongoose to the database
+mongoose.connect(db, function(error) {
+    if (error) {
+        console.log(error);
+    }
+
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+app.listen(PORT, function() {
+    console.log("Listening on port:" + PORT);
+});
